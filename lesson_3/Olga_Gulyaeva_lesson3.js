52a206606a0e53a8676731675d54d78dd31bb473
olga121 <igolv2003@yahoo.com>
2016-07-23 13:21:22 +0100

    lesson 3, even and odd numbers in range of 1000, while loop, etc

---
+++ lesson_3/Olga_Gulyaeva_lesson3.js
@@ -0,0 +1,44 @@
+﻿// Ольга Гуляева - урок 2
+
+"use strict"
+
+//var x;
+//
+//	while (x !== null){	
+
+function CheckNumber(){
+	var number = document.getElementById("number").value;
+	if (!isNaN(number)){
+		if(isPrime(number)){
+			document.getElementById("result").innerHTML = number + " простое число!";
+		} else { document.getElementById("result").innerHTML = number + " не простое число";
+	} else { document.getElementById("result").innerHTML = "Пожалуйта, введите число";
+	}
+}
+
+
+
+
+
+
+
+
+// Max prime number in range from 0 till 1000000
+
+function MaxprimeNumber(){
+	var MaxPrimeNumber = 3;
+	for (var i = 4; i <= 1000000; i++){
+		for (var k = 2; k <= Math.sqrt(i); k++){
+			if (i % k == 0){
+				x = false;
+				break;
+			}
+			var x = true;
+		}
+		if(x){
+			MaxPrimeNumber = i;
+		}
+	}		
+	alert ("Максимальное простое число " + MaxPrimeNumber)
+}
+MaxprimeNumber()
